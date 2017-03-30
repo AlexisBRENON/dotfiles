b8cd1293eccc03e329edd40edefe625ff40f793b
Alexis BRENON <brenon.alexis@gmail.com>
2017-03-30 15:58:47 +0200

    💻  Define path to custom latex install

--- shell_startup_script/posix/host/hector2.sh
+++ shell_startup_script/posix/host/hector2.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
 . "${HOME}/torch/install/bin/torch-activate"
+
+export TEXMFHOME="${HOME}/.local/share/texmf"
+export PATH="/home/brenon/.local/texlive/2016/bin/x86_64-linux:${PATH}"
+export PATH="/home/brenon/.local/bin:${PATH}"
