67eea35332666b4fc0ecbced8f79a6e032afdbe3
Alexis BRENON <brenon.alexis@gmail.com>
2017-04-06 08:53:41 +0200

    ✨  Update theme prompt for BASH/ZSH compatibility
    Prompt theme is the same on bash and zsh !!!!

--- shell_startup_script/bash/environment/prompt.sh
+++ shell_startup_script/bash/environment/prompt.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-source_file "${HOME}/.config/shell/posix/plugin/prompt_alexis/prompt_alexis_setup.bash"
-prompt_alexis_setup
+source_file "${HOME}/.config/shell/posix/plugin/prompt_alexis/prompt_alexis_setup"
