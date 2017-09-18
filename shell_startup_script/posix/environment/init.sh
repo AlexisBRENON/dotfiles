375906ccdac61f4e050e311f81a91acf6ede6ebc
Alexis BRENON <brenon.alexis@gmail.com>
2017-09-18 15:30:53 +0200

    ✨  add some hector2 specific config

--- shell_startup_script/posix/environment/init.sh
+++ shell_startup_script/posix/environment/init.sh
@@ -11,5 +11,6 @@ source_file "${ENV_CONFIG_PATH}/ls.sh"
 source_file "${ENV_CONFIG_PATH}/less.sh"
 source_file "${ENV_CONFIG_PATH}/display.sh"
 source_file "${ENV_CONFIG_PATH}/colors.sh"
+source_file "${ENV_CONFIG_PATH}/lua.sh"
 
 unset ENV_CONFIG_PATH
--- shell_startup_script/posix/host/hector2.sh
+++ shell_startup_script/posix/host/hector2.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
 . "${HOME}/torch/install/bin/torch-activate"
+export LUA_PATH="./?.lua;./?/init.lua;$LUA_PATH"
 
-export TEXMFHOME="${HOME}/.local/share/texmf"
+export TEXMFHOME="${HOME}/.local/texlive/2016/texmf-dist/"
 export PATH="/home/brenon/.local/texlive/2016/bin/x86_64-linux:${PATH}"
 export PATH="/home/brenon/.local/bin:${PATH}"
+
+export ARCADES_HOME="/home/brenon/Documents/thesis/SmartHome/Arcades/src"
+arcades() {
+  # Expand ARCADES_HOME on call
+  TREPL_HISTORY="${TREPL_HISTORY:-${ARCADES_HOME}/.history}" th -i -e "package.path = '${ARCADES_HOME}/src/?.lua;${ARCADES_HOME}/src/?/init.lua;${ARCADES_HOME}/?/init.lua;' .. package.path"
+}
