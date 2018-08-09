4a60ed14f351eecd0be5fa2df1ccb65cd7e9e995
Alexis BRENON <brenon.alexis@gmail.com>
2018-08-09 15:25:41 +0200

    ✨  Discover local Latex binaries

--- shell_startup_script/posix/environment/latex.sh
+++ shell_startup_script/posix/environment/latex.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 export TEXMFHOME="${HOME}/.local/share/texmf"
+
+if [ -d "${HOME}/.local/texlive/2018/bin" ]; then
+  export PATH="${HOME}/.local/texlive/2018/bin/x86_64-linux:${HOME}/.local/texlive/2018/bin:${PATH}"
+fi
