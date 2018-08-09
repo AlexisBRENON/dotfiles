2b1f66c04b78ee298aa16318c1835c307f6f3a80
Alexis BRENON <brenon.alexis@gmail.com>
2018-08-09 15:34:01 +0200

    ⬆️  Rely on the new God Bless Git repo

--- shell_startup_script/posix/plugin/init.sh
+++ shell_startup_script/posix/plugin/init.sh
@@ -4,6 +4,6 @@ PLUGIN_CONFIG_PATH="$(dirname "$RC_SOURCED_FILE")"
 
 INFO "Loading plugins for generic shells"
 
-source_file "${PLUGIN_CONFIG_PATH}/god-bless-git/init.sh"
+source_file "${PLUGIN_CONFIG_PATH}/god-bless-git/god_bless_git.sh"
 
 unset PLUGIN_CONFIG_PATH
