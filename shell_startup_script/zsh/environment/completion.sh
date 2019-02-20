1aa9b6fbbc49b835cf0ffd87e8fc73825d556e37
Alexis BRENON <brenon.alexis@gmail.com>
2019-02-20 14:16:50 +0100

    Add tensorflow completion

--- shell_startup_script/zsh/environment/completion.sh
+++ shell_startup_script/zsh/environment/completion.sh
@@ -6,25 +6,24 @@ INFO "Loading completion for Zsh."
 
 # The following lines were added by compinstall
 
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 zstyle ':completion:*' completer _expand _complete _ignored
 zstyle ':completion:*' expand suffix
 zstyle ':completion:*' ignore-parents parent directory
 zstyle ':completion:*' insert-unambiguous true
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
 zstyle ':completion:*' list-suffixes true
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '' '' 'l:|=* r:|=*'
-zstyle ':completion:*' menu select=0
+zstyle ':completion:*' menu select=long
 zstyle ':completion:*' preserve-prefix '//[^/]##/'
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
 zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' verbose true
 zstyle ':completion:*' use-cache on # Enable cache use for completion
-
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
-
-zstyle :compinstall filename "${HOME}/.config/shell/zsh/environment/completion.sh"
+zstyle ':completion:*' verbose true
+zstyle ':completion:*:*:display-pb:*' file-patterns '*.pb:protobuf-file'
+zstyle :compinstall filename '/home/alexis//.config/shell/zsh/environment/completion.sh'
 
 autoload -Uz compinit
 compinit
