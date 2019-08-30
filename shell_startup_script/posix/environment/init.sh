16e955dd32168e302394f1bd4ec2ede478ea6cc1
Alexis BRENON <alexis.brenon@adyoulike.com>
2019-08-30 18:41:54 +0200

    Handle neovim editor

--- shell_startup_script/posix/environment/init.sh
+++ shell_startup_script/posix/environment/init.sh
@@ -5,7 +5,12 @@ ENV_CONFIG_PATH="$(dirname "$RC_SOURCED_FILE")"
 INFO "Loading environment variables for generic shell."
 
 export WORDCHARS=""
-export EDITOR="vim"
+if command -pv nvim >/dev/null; then
+	EDITOR="nvim"
+elif command -pv vim >/dev/null; then
+	EDITOR="vim"
+fi
+export EDITOR
 
 source_submodules "${ENV_CONFIG_PATH}"
 
